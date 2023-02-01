a375806da4485126838888654a236a8e9386dccb
Asif Nawaz <assu.nawaz007@gmail.com>
2023-02-01 16:05:28 +0500

    added ant-design into homePage

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import DefaultLayout from '../components/DefaultLayout'
 
 function HomePage() {
   return (
-    <div>
-      <h1>HomePage</h1>
-    </div>
+    <>
+    <DefaultLayout/>
+    </>
   )
 }
 
